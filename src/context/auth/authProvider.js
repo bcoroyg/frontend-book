411f1c198675f1d6a1b7727c361ca9448c707d59
Emilio Coroy <becgonzalez25@gmail.com>
2023-01-06 13:43:05 -0600

    protegiendo ruta de dashboard lista de libros

--- src/context/auth/authProvider.js
+++ src/context/auth/authProvider.js
@@ -2,9 +2,14 @@ import { useState } from "react";
 import authContext from "./authContext";
 
 const AuthProvider = (props) => {
+
+  //Obtener el token de localstorage
+  //const token = localStorage.getItem("token");
+
   // definir el state inicial
   const [auth, setAuth] = useState({
-    token: "",
+    //token: token ? token : "",
+    token: '',
     auth: false,
   });
 
--- src/pages/admin/ListBooks.jsx
+++ src/pages/admin/ListBooks.jsx
@@ -1,4 +1,21 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/auth";
+
 const ListBooks = () => {
+  //useNavigate
+  const navigate = useNavigate();
+  //Utilizar valores del context
+  const [auth, setAuth] = useContext(AuthContext);
+
+  useEffect(() => {
+    if (auth.token !== "") {
+      
+    } else {
+      return navigate("/login");
+    }
+  }, [auth.token, navigate]);
+
   return <h1>Tabla de libros</h1>;
 };
 
