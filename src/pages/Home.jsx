a67a59a32fc09dbbc5df25001eff1a678406f0a3
Emilio Coroy <becgonzalez25@gmail.com>
2023-01-05 19:10:44 -0600

    page home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <div className="bg-secondary bg-opacity-10 p-5 rounded text-center">
+      <h1 className="display-4 text-center">¡Web Libros!</h1>
+      <p className="lead">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non neque
+        temporibus doloremque voluptatum dolorum veritatis amet incidunt ut
+        ducimus pariatur!.
+      </p>
+      <hr className="my-4" />
+    </div>
+  );
+};
+
+export default Home;
